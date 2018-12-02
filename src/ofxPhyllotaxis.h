b6e5dd3c9c35da79399323d88d6e4c36bdf4001a
Davide Prati <161152+edap@users.noreply.github.com>
2018-12-02 15:09:09 +0100

    Update ofxPhyllotaxis.h

--- src/ofxPhyllotaxis.h
+++ src/ofxPhyllotaxis.h
@@ -1,28 +1,28 @@
 #pragma once
-#include "math/ofVec3f.h"
+#include "ofMain.h"
 
 class ofxPhyllotaxis{
     public:
-    static ofVec3f simple(const int i, const float angleInRadians,
+    static glm::vec3 simple(const int i, const float angleInRadians,
                           const float spread){
         float currentAngle = i * angleInRadians;
         float radius = spread * sqrt(i);
         float x = radius * cos(currentAngle);
         float z = radius * sin(currentAngle);
-        return ofVec3f(x,0.0,z);
+        return glm::vec3(x,0.0,z);
     }
 
-    static ofVec3f conical(const int i, const float angleInRadians,
+    static glm::vec3 conical(const int i, const float angleInRadians,
                            const float spread, const float extrude){
         float currentAngle = i * angleInRadians;
         float radius = spread * sqrt(i);
         float x = radius * cos(currentAngle);
         float z = radius * sin(currentAngle);
         float y = i * (extrude) * -1;
-        return ofVec3f(x,y,z);
+        return glm::vec3(x,y,z);
     }
 
-    static ofVec3f apple(const int i, const float angleInRadians,
+    static glm::vec3 apple(const int i, const float angleInRadians,
                          const float spread, const int total){
         float inc = PI / float(total);
         float currentAngleA = i * inc;
@@ -31,6 +31,6 @@ class ofxPhyllotaxis{
         float x = radius * sin(currentAngleA) * cos(currentAngleB);
         float z = radius * sin(currentAngleA) * sin(currentAngleB);
         float y = radius * cos(currentAngleA);
-        return ofVec3f(x,y,z);
+        return glm::vec3(x,y,z);
     }
 };
